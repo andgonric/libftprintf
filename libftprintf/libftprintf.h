b90f970cac404b4cba51517d9e0a8c74e887ce7a
Andre Goncalves Ricardo <andgonca@c1r15s9.42lisboa.com>
2022-11-15 22:13:40 +0000

    envio v1

---
+++ libftprintf/libftprintf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andgonca <andgonca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 21:08:08 by andgonca          #+#    #+#             */
+/*   Updated: 2022/11/15 21:38:53 by andgonca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *, ...);
+#endif
