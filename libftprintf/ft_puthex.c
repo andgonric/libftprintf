f33765720ec05310b8599e0c5cf93accad3d2c0b
Andre Goncalves Ricardo <andgonca@c1r15s9.42lisboa.com>
2022-11-19 18:07:43 +0000

    envio v3

--- libftprintf/ft_puthex.c
+++ libftprintf/ft_puthex.c
@@ -6,11 +6,27 @@
 /*   By: andgonca <andgonca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 17:17:17 by andgonca          #+#    #+#             */
-/*   Updated: 2022/11/19 17:21:16 by andgonca         ###   ########.fr       */
+/*   Updated: 2022/11/19 18:05:46 by andgonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+int	pot(unsigned int num)
+{
+	unsigned int	pot;
+	unsigned int	
+
+	pot = 1;
+	while (pot < num)
+	{
+		if ((pot * 16) < num)
+			pot *= 16;
+		else
+			break;
+	}
+	return (pot);
+}
 
 int	ft_puthex(unsigned int num)
 {
